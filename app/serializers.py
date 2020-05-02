f486a517874acc224b2c6e46023a29c97985a91b
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2020-05-02 18:47:45 +0530

    showing error when negative number or fraction is entered as quantity

--- app/serializers.py
+++ app/serializers.py
@@ -3,3 +3,11 @@ from rest_framework import serializers
 
 class OrderSerializer(serializers.Serializer):
     order = serializers.ListField(child=serializers.FloatField())
+
+    def validate_order(self, order):
+        for x in order:
+            if x<0:
+                raise serializers.ValidationError("Error: Quantity can't be neagative.")
+            elif (not(x%1==0)):
+                raise serializers.ValidationError("Error: Quantity can't be a fraction.")
+        return order
