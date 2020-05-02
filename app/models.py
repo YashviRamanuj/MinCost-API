1c5ab2a2332a960b7130e9ac54d3cb31877828e9
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2020-05-02 07:31:08 +0530

    added model for Order

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    qA = models.FloatField(blank=True, default=0)  # quantity of item A in the order
+    qB = models.FloatField(blank=True, default=0)
+    qC = models.FloatField(blank=True, default=0)
+    qD = models.FloatField(blank=True, default=0)
+    qE = models.FloatField(blank=True, default=0)
+    qF = models.FloatField(blank=True, default=0)
+    qG = models.FloatField(blank=True, default=0)
+    qH = models.FloatField(blank=True, default=0)
+    qI = models.FloatField(blank=True, default=0)
